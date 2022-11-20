126f05f71e602fd9270073f1275cc98d2357deca
Alisher Kenzhebayev <alisher.kenzhebayev@alumni.nu.edu.kz>
2022-11-20 12:53:36 +0100

    Initial single_bounce

---
+++ src/materials.hpp
@@ -0,0 +1,78 @@
+#pragma once 
+
+#include <utility>
+#include <tuple>
+#include <stdexcept>
+#include "math.hpp"
+#include "rng.hpp"
+
+class Material
+{
+public:
+    Material()
+    {
+        Reset();
+    }
+
+    void Reset()
+    {
+        mDiffuseReflectance = Vec3f(0);
+        mPhongReflectance   = Vec3f(0);
+        mPhongExponent      = 1.f;
+    }
+
+    /**
+     * Randomly chooses an outgoing direction that is reflected from the material surface
+     * Arguments:
+     *  - incomingDirection = a normalized direction towards the previous (origin) point in the scene
+     *  - rng = random generator
+     * Returns:
+     *  - a randomly sampled reflected outgoing direction
+     *  - the intensity corresponding to the reflected light
+     *  - the probability density (PDF) of choosing this direction
+     */
+    std::tuple<Vec3f, Vec3f, float> SampleReflectedDirection(const Vec3f& incomingDirection, Rng& rng) const {
+        throw std::logic_error("Not implemented");
+    }
+
+    /**
+     * Returns the probability density corresponding to sampleReflectedDirection,
+     * i.e., what is the probability that calling sampleReflectedDirection would randomly choose the given outgoingDirection
+     * Arguments:
+     *  - incomingDirection = a normalized direction towards the previous (origin) point in the scene
+     *  - outgoingDirection = the randomly sampled (normalized) outgoing direction
+     */
+    float PDF(const Vec3f& incomingDirection, const Vec3f& outgoingDirection) const {
+        throw std::logic_error("Not implemented");
+    }
+
+    /**
+     * Returns the intensity corresponding to the reflected light according to this material's BRDF
+     * Arguments:
+     *  - incomingDirection = a normalized direction towards the previous (origin) point in the scene
+     *  - outgoingDirection = a normalized outgoing reflected direction
+     */
+    Vec3f EvaluateBRDF(const Vec3f& incomingDirection, const Vec3f& outgoingDirection) const {
+        if (incomingDirection.z <= 0 && outgoingDirection.z <= 0) {
+			return Vec3f(0);
+        }
+
+        /*if()
+        printf("D: %f %f %f | S: %f %f %f", mDiffuseReflectance.x, mDiffuseReflectance.y, mDiffuseReflectance.z, 
+            mPhongReflectance.x, mPhongReflectance.y, mPhongReflectance.z);*/
+
+		Vec3f diffuseComponent = mDiffuseReflectance / PI_F;
+
+        Vec3f N = Vec3f(0.0f, 0.0f, 1.0f);
+        Vec3f R = Normalize(2 * Dot(N, incomingDirection) * N - incomingDirection);
+        float dotPow = pow(Dot(outgoingDirection, R), mPhongExponent);
+        Vec3f glossyComponent = mPhongReflectance * dotPow * (mPhongExponent + 2) / (2 * PI_F);
+
+		Vec3f outIntensity = (diffuseComponent + glossyComponent);
+        return (outIntensity);
+    }
+
+    Vec3f mDiffuseReflectance;
+    Vec3f mPhongReflectance;
+    float mPhongExponent;
+};
---
+++ src/utils.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <vector>
+#include <cmath>
+#include <utility>
+#include "math.hpp"
+#include "rng.hpp"
+
+#define EPSILON_COSINE 1e-6f
+#define EPSILON_RAY    1e-3f
+
+// sRGB luminance
+float Luminance(const Vec3f& aRGB)
+{
+    return 0.212671f * aRGB.x +
+        0.715160f * aRGB.y +
+        0.072169f * aRGB.z;
+}
+
+// reflect vector through (0,0,1)
+Vec3f ReflectLocal(const Vec3f& aVector)
+{
+    return Vec3f(-aVector.x, -aVector.y, aVector.z);
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Utilities for converting PDF between Area (A) and Solid angle (W)
+// WtoA = PdfW * cosine / distance_squared
+// AtoW = PdfA * distance_squared / cosine
+
+float PdfWtoA(
+    const float aPdfW,
+    const float aDist,
+    const float aCosThere)
+{
+    return aPdfW * std::abs(aCosThere) / Sqr(aDist);
+}
+
+float PdfAtoW(
+    const float aPdfA,
+    const float aDist,
+    const float aCosThere)
+{
+    return aPdfA * Sqr(aDist) / std::abs(aCosThere);
+}
+
+/// <summary>
+/// Given the triangle and rng, return a random point on that triangle
+/// </summary>
+Vec3f PointOnTriangle(Vec3f p0, Vec3f e1, Vec3f e2, Rng& rng) {
+    float u = rng.GetFloat();
+    float v = rng.GetFloat();
+    Vec3f B = p0 + e1;
+    Vec3f C = p0 + e2;
+
+    float su0 = sqrt(u);
+    return (1 - su0) * B + (v * su0) * C + (1-v) * su0 * p0;
+}
+
+Vec3f PointOnSphere(float radius, Rng& rng) {
+    Vec2f uv = rng.GetVec2f();
+
+    float z = 1 - 2 * uv.x;
+    float r = sqrt(std::max(0.f, 1.f - z * z));
+    float phi = 2 * PI_F * uv.y;
+    return radius * Vec3f(r * cos(phi), r * sin(phi), z);
+}
